101b2c6c4c639e1317c99bed639c8af587631856
Cyrille Carpentier <cyrille.carpentier@imie.fr>
2019-07-11 11:02:36 +0200

    first build

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void _LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+
+}
